1c2be562bc3e3be06cbf1a40a4907c15851ab8c0
Ricardo Melida <ricardomelida92@gmail.com>
2024-09-13 07:40:52 -0400

    added new tests

---
+++ src/test/server_app2/test_utils/RequestTestWrapper.ts
@@ -0,0 +1,20 @@
+import { HTTP_METHODS } from "../../../app/server_app/model/ServerModel";
+
+export class RequestTestWrapper {
+  public body: object | undefined;
+  public method: HTTP_METHODS | undefined;
+  public url: string | undefined;
+  public headers:Record<string, string> = {}
+
+  public on(event: string, cb: Function) {
+    if (event == "data") cb(JSON.stringify(this.body));
+    else cb();
+  }
+
+  public clearFields() {
+    this.body = undefined;
+    this.method = undefined;
+    this.url = undefined;
+    this.headers = {};
+  }
+}
